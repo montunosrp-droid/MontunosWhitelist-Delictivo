4bf6ecc9e2a70690cb365067704d3ccd9946a840
montunosrp-droid <montunosrp@gmail.com>
2025-12-18 22:17:49 -0500

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -42,7 +42,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       secure: process.env.NODE_ENV === "production",
-      httpOnly: true,
+      sameSite: "lax",
       maxAge: 24 * 60 * 60 * 1000,
     },
   }),
